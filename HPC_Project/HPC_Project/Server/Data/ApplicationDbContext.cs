791db5e857d40dd729dcc0cb1c979f97c5d27717
Tyler McMurray <tylermcmuffin5@gmail.com>
2023-03-22 11:00:33 -0600

    Init
    Initial push of project

---
+++ HPC_Project/HPC_Project/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using HPC_Project.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HPC_Project.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
