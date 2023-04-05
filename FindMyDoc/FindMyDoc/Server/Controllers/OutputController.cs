72613612b37ef7ca67db0e5be1dca8af1ce8706e
Kireet Pajjur <963124@cognizant.com>
2023-04-05 14:00:51 -0500

    Possible Output Page

---
+++ FindMyDoc/FindMyDoc/Server/Controllers/OutputController.cs
@@ -0,0 +1,47 @@
+﻿using FindMyDoc.Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindMyDoc.Server.Controllers
+{
+    [Authorize]
+    public class OutputController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public OutputController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            // Get the current user's ID
+            var userId = _signInManager.UserManager.GetUserId(User);
+
+            // Retrieve the user's email and phone number
+            var user = await _userManager.FindByIdAsync(userId);
+            var username = user.UserName;
+            var firstname = user.firstName;
+            var lastname = user.lastName;
+            var dob = user.dob;
+            var region = user.county;
+            var insurance = user.insuranceNum;
+
+            var model = new ApplicationUser
+            {
+                UserName = username,
+                firstName = firstname,
+                lastName = lastname,
+                dob = dob,
+                county = region,
+                insuranceNum = insurance
+            };
+
+            return new JsonResult(model);
+        }
+    }
+}
---
+++ FindMyDoc/FindMyDoc/Shared/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FindMyDoc.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+       
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //public date dob { get; set; }
+        [Column(TypeName = "Date")] 
+        public DateTime dob { get; set; }
+        public string address { get; set; }
+        public string insuranceNum { get; set; }
+        public string? fips { get; set; }
+        public string gender { get; set; }
+        public DateTime date { get; set; }
+        public string state { get; set; }
+        public string county { get; set; }
+    }
+}
